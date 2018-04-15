1d168f984bfede655d43a6d7c2cc547d2ff0deeb
yoni_Sku <yoni.sku@gmail.com>
2018-04-15 19:34:34 +0300

    Sorted structure even more

---
+++ EmoteTool/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace EmoteTool.ViewModels
+{
+    internal class MainWindowViewModel
+    {
+        //Window Size
+        public double WindowHeight { get; set; } = 500;
+        public double WindowWidth { get; set; } = 300;
+        
+        public Dictionary<string, int> Numbers { get; set; }
+
+        public MainWindowViewModel()
+        {
+            Numbers = new Dictionary<string, int>
+            {
+                {"One", 1},
+                {"Two", 2},
+            };
+        }
+    }
+}
