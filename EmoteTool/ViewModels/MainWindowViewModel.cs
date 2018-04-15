aea99d524d9b094a455f6e2ecdc508bb1d88e466
yoni_Sku <yoni.sku@gmail.com>
2018-04-15 21:45:37 +0300

    Revert "Revert "IGNORE AND AVOID, was a reset of the whole thing for testing""
    This reverts commit 410e5138f3eb35a01b1dd79fa7ca7418ccfbc3e7.

--- EmoteTool/ViewModels/MainWindowViewModel.cs
+++ EmoteTool/ViewModels/MainWindowViewModel.cs
@@ -1,22 +1,8 @@
-﻿using System.Collections.Generic;
-
-namespace EmoteTool.ViewModels
+﻿namespace EmoteTool.ViewModels
 {
     internal class MainWindowViewModel
     {
-        //Window Size
-        public double WindowHeight { get; set; } = 500;
-        public double WindowWidth { get; set; } = 300;
-        
-        public Dictionary<string, int> Numbers { get; set; }
-
-        public MainWindowViewModel()
-        {
-            Numbers = new Dictionary<string, int>
-            {
-                {"One", 1},
-                {"Two", 2},
-            };
-        }
+        public int height { get; set; } = 1000;
+        public int width { get; set; } = 300;
     }
 }
