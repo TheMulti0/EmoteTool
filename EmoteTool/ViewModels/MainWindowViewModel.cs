3679d6ee53a32c23fc8a11416abf627700ae05ea
yoni_Sku <yoni.sku@gmail.com>
2018-04-17 21:12:10 +0300

    im not actually sure exactly what this is, probably some more testing of mine, which is as hopeless as it is worthless

--- EmoteTool/ViewModels/MainWindowViewModel.cs
+++ EmoteTool/ViewModels/MainWindowViewModel.cs
@@ -5,18 +5,12 @@ namespace EmoteTool.ViewModels
     internal class MainWindowViewModel
     {
         //Window Size
-        public double WindowHeight { get; set; } = 500;
-        public double WindowWidth { get; set; } = 300;
-        
-        public Dictionary<string, int> Numbers { get; set; }
+        public double WindowHeight { get; set; } = 400;
+        public double WindowWidth { get; set; } = 600;
 
         public MainWindowViewModel()
         {
-            Numbers = new Dictionary<string, int>
-            {
-                {"One", 1},
-                {"Two", 2},
-            };
+            
         }
     }
 }
