10665fee6a58a9b23f6da49f178cbe0c746f70de
TheMulti0 <raphael@codeprecise.com>
2018-04-19 23:28:57 +0300

    Fixed sizing problem

--- EmoteTool/ViewModels/MainWindowViewModel.cs
+++ EmoteTool/ViewModels/MainWindowViewModel.cs
@@ -7,6 +7,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using Microsoft.Win32;
+using Size = System.Drawing.Size;
 
 namespace EmoteTool.ViewModels
 {
@@ -20,13 +21,13 @@ namespace EmoteTool.ViewModels
         
         public BitmapImage ImageSource { get; set; }
 
-        public Collection<BitmapImage> Images { get; set; }
+        public ObservableCollection<BitmapSource> Images { get; set; }
 
-        public BitmapImage SelectedItem { get; set; }
+        public BitmapSource SelectedItem { get; set; }
 
         public MainWindowViewModel()
         {
-            CopyCommand = new Command(() => Clipboard.SetImage(SetSize(SelectedItem)));
+            CopyCommand = new Command(() => Clipboard.SetImage(SelectedItem));
 
             AddCommand = new Command(Open);
 
@@ -34,7 +35,7 @@ namespace EmoteTool.ViewModels
 
             ImageSource = new BitmapImage(new Uri("../Emoji.jpg", UriKind.Relative));
 
-            Images = new ObservableCollection<BitmapImage>
+            Images = new ObservableCollection<BitmapSource>
             {
                 ImageSource, ImageSource, ImageSource
             };
@@ -51,17 +52,41 @@ namespace EmoteTool.ViewModels
             };
             if (op.ShowDialog() == true)
             {
-                var image = new BitmapImage(new Uri(op.FileName));
-                Images.Add(image);
+                var bitmap = Image.FromFile(op.FileName);
+                var resized = ResizeImage(bitmap, new Size(35, 35));
+                var bitmapSource = ToBitmapSource(resized);
+                Images.Add(bitmapSource);
             }
         }
 
-        public BitmapImage SetSize(BitmapImage image)
+        public BitmapSource ToBitmapSource(Bitmap src)
         {
-            image.Clone();
+            var ms = new MemoryStream();
+            ((System.Drawing.Bitmap)src).Save(ms, System.Drawing.Imaging.ImageFormat.Bmp);
+            BitmapImage image = new BitmapImage();
             image.BeginInit();
+            ms.Seek(0, SeekOrigin.Begin);
+            image.StreamSource = ms;
             image.EndInit();
             return image;
+        }
+
+        public static Bitmap ResizeImage(Image imgToResize, Size size)
+        {
+            try
+            {
+                Bitmap b = new Bitmap(size.Width, size.Height);
+                using (Graphics g = Graphics.FromImage(b))
+                {
+                    g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+                    g.DrawImage(imgToResize, 0, 0, size.Width, size.Height);
+                }
+                return b;
+            }
+            catch
+            {
+                return null;
+            }
         }
     }
 }
