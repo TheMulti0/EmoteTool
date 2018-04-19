83e74784076555bcccba30df8765fbf53124ca69
yoni_Sku <yoni.sku@gmail.com>
2018-04-19 20:20:20 +0300

    Added lists of images and option to choose custom image

--- EmoteTool/ViewModels/MainWindowViewModel.cs
+++ EmoteTool/ViewModels/MainWindowViewModel.cs
@@ -1,21 +1,57 @@
 ﻿using System;
+using System.Collections.ObjectModel;
 using System.Windows;
 using System.Windows.Input;
 using System.Windows.Media.Imaging;
+using Microsoft.Win32;
 
 namespace EmoteTool.ViewModels
 {
     internal class MainWindowViewModel
     {
-        public ICommand ButtonCommand { get; set; }
+        public ICommand CopyCommand { get; set; }
+
+        public ICommand AddCommand { get; set; }
+
+        public ICommand OpenCommand { get; set; }
 
         public BitmapImage ImageSource { get; set; }
 
+        public Collection<BitmapImage> Images { get; set; }
+
+        public BitmapImage SelectedItem { get; set; }
+
         public MainWindowViewModel()
         {
-            ButtonCommand = new Command(() => Clipboard.SetImage(ImageSource));
+            CopyCommand = new Command(() => Clipboard.SetImage(SelectedItem));
+
+            AddCommand = new Command(image => Images.Add((BitmapImage) image));
+
+            OpenCommand = new Command(Open);
 
             ImageSource = new BitmapImage(new Uri("../Emoji.jpg", UriKind.Relative));
+
+            Images = new ObservableCollection<BitmapImage>
+            {
+                ImageSource
+            };
+        }
+
+        public void Open()
+        {
+            BitmapImage img = null;
+            OpenFileDialog op = new OpenFileDialog
+            {
+                Title = "Select an image",
+                Filter = "All supported graphics|*.jpg;*.jpeg;*.png|" +
+                         "JPEG (*.jpg;*.jpeg)|*.jpg;*.jpeg|" +
+                         "Portable Network Graphic (*.png)|*.png"
+            };
+            if (op.ShowDialog() == true)
+            {
+                img = new BitmapImage(new Uri(op.FileName));
+                AddCommand.Execute(img);
+            }
         }
     }
 }
