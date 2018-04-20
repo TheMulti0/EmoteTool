4b22bd63dd31342e7332e299e7e6cd7462c5c01f
yoni_Sku <yoni.sku@gmail.com>
2018-04-20 17:31:28 +0300

    A lot of stuff, including text beneath image, and semi-doubleclick copy function

---
+++ EmoteTool/ViewModels/EmoteItem.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows.Media.Imaging;
+
+namespace EmoteTool.ViewModels
+{
+    internal class EmoteItem
+    {
+        public string Name { get; set; }
+
+        public BitmapSource Image { get; set; }
+
+
+        public EmoteItem(string name, BitmapSource image)
+        {
+            Name = name;
+            Image = image;
+        }
+    }
+}
--- EmoteTool/ViewModels/MainWindowViewModel.cs
+++ EmoteTool/ViewModels/MainWindowViewModel.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using System.Collections.ObjectModel;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Drawing.Imaging;
 using System.IO;
+using System.Linq;
 using System.Windows;
 using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using Microsoft.Win32;
 using Size = System.Drawing.Size;
@@ -14,59 +15,86 @@ namespace EmoteTool.ViewModels
 {
     internal class MainWindowViewModel
     {
-        public ICommand CopyCommand { get; set; }
-         
         public ICommand AddCommand { get; set; }
 
         public ICommand RemoveCommand { get; set; }
-        
-        public BitmapImage ImageSource { get; set; }
 
-        public ObservableCollection<BitmapSource> Images { get; set; }
+        public ICommand CopyCommand { get; set; }
 
-        public BitmapSource SelectedItem { get; set; }
+        public ObservableCollection<EmoteItem> Emotes { get; set; }
+
+        public static EmoteItem SelectedItem { get; set; }
+
+        public string EmoteName { get; set; }
 
         public Size IconSize { get; set; }
 
+        private readonly string _emoteDefault;
+
+
         public MainWindowViewModel()
         {
-            CopyCommand = new Command(() => Clipboard.SetImage(SelectedItem));
+            AddCommand = new Command(SelectImage);
 
-            AddCommand = new Command(Open);
+            RemoveCommand = new Command(() => Emotes.Remove(SelectedItem));
 
-            RemoveCommand = new Command(() => Images.Remove(SelectedItem));
-
-            ImageSource = new BitmapImage(new Uri("../Emoji.jpg", UriKind.Relative));
-
-            Images = new ObservableCollection<BitmapSource>
+            CopyCommand = new Command((item) =>
             {
-                ImageSource, ImageSource, ImageSource
-            };
+                if (SelectedItem == null)
+                {
+                    SelectedItem = (EmoteItem) item;
+                }
+                Clipboard.SetImage(SelectedItem.Image);
+            });
 
-            IconSize = new Size();
+            Emotes = new ObservableCollection<EmoteItem>();
+
+            _emoteDefault = "Enter an emote string here";
+            EmoteName = _emoteDefault;
+
+            IconSize = new Size(35, 35);
         }
 
-        public void Open()
+        public void SelectImage()
         {
-            var op = new OpenFileDialog
+            var dialog = new OpenFileDialog
             {
                 Title = "Select an image",
                 Filter = "All supported graphics|*.jpg;*.jpeg;*.png|" +
                          "JPEG (*.jpg;*.jpeg)|*.jpg;*.jpeg|" +
                          "Portable Network Graphic (*.png)|*.png"
             };
-            if (op.ShowDialog() == true)
+            if (dialog.ShowDialog() == true)
             {
-                Image bitmap = Image.FromFile(op.FileName);
-                Bitmap resized = ResizeImage(bitmap, IconSize);
-                BitmapSource bitmapSource = BitmapToBitmapSource(resized);
-                Images.Add(bitmapSource);
+                var image = Image.FromFile(dialog.FileName);
+                var resized = ResizeBitmap(image, IconSize);
+                var bitmapSource = BitmapToBitmapSource(resized);
+
+                if (EmoteName == _emoteDefault || Emotes.Any(emote => EmoteName == emote.Name))
+                {
+                    var number = Emotes.Count + 1;
+                    EmoteName = "Emote #" + number;
+                }
+
+                Emotes.Add(new EmoteItem(EmoteName, bitmapSource));
             }
+        }
+
+        public static Bitmap ResizeBitmap(Image imgToResize, Size size)
+        {
+            var b = new Bitmap(size.Width, size.Height);
+            using (var g = Graphics.FromImage(b))
+            {
+                g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                g.DrawImage(imgToResize, 0, 0, size.Width, size.Height);
+            }
+
+            return b;
         }
 
         public BitmapSource BitmapToBitmapSource(Bitmap bitmap)
         {
-            using (MemoryStream stream = new MemoryStream())
+            using (var stream = new MemoryStream())
             {
                 bitmap.Save(stream, ImageFormat.Png);
                 stream.Position = 0;
@@ -80,24 +108,6 @@ namespace EmoteTool.ViewModels
                 result.Freeze();
 
                 return result;
-            }
-        }
-
-        public static Bitmap ResizeImage(Image imgToResize, Size size)
-        {
-            try
-            {
-                Bitmap b = new Bitmap(size.Width, size.Height);
-                using (Graphics g = Graphics.FromImage(b))
-                {
-                    g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
-                    g.DrawImage(imgToResize, 0, 0, size.Width, size.Height);
-                }
-                return b;
-            }
-            catch
-            {
-                return null;
             }
         }
     }
