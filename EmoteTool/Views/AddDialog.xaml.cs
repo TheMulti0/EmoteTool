5812c839dfcc089b35a6b5160d38abc760e01587
TheMulti0 <raphael@codeprecise.com>
2018-05-02 17:04:01 +0300

    Extracted AddDialog

---
+++ EmoteTool/Views/AddDialog.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+using MaterialDesignThemes.Wpf;
+
+namespace EmoteTool.Views
+{
+    /// <summary>
+    ///     Interaction logic for AddDialog.xaml
+    /// </summary>
+    public partial class AddDialog : DialogHost
+    {
+        public AddDialog()
+        {
+            InitializeComponent();
+
+            var window = Application.Current.MainWindow;
+            DataContext = window?.DataContext;
+        }
+    }
+}
