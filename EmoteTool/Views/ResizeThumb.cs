71fac7a22e6c897b8f219b7e9f2a01942230bcd1
yoni_Sku <yoni.sku@gmail.com>
2018-05-17 23:32:22 +0300

    reszing 0.0.0.0.0.001

---
+++ EmoteTool/Views/ResizeThumb.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+
+namespace EmoteTool.Views
+{
+    public class ResizeThumb : Thumb
+    {
+        public ResizeThumb()
+        {
+            DragDelta += ResizeThumb_DragDelta;
+        }
+
+        private void ResizeThumb_DragDelta(object sender, DragDeltaEventArgs e)
+        {
+            var item = this.DataContext as Control;
+
+
+            if (item != null)
+            {
+                double deltaVertical, deltaHorizontal;
+
+                switch (VerticalAlignment)
+                {
+                    case VerticalAlignment.Bottom:
+                        deltaVertical = Math.Min(-e.VerticalChange,
+                            item.ActualHeight - item.MinHeight);
+                        item.Height -= deltaVertical;
+                        break;
+                    case VerticalAlignment.Top:
+                        deltaVertical = Math.Min(e.VerticalChange,
+                            item.ActualHeight - item.MinHeight);
+                        Canvas.SetTop(item, Canvas.GetTop(item) + deltaVertical);
+                        item.Height -= deltaVertical;
+                        break;
+                }
+
+                switch (HorizontalAlignment)
+                {
+                    case HorizontalAlignment.Left:
+                        deltaHorizontal = Math.Min(e.HorizontalChange,
+                            item.ActualWidth - item.MinWidth);
+                        Canvas.SetLeft(item, Canvas.GetLeft(item) + deltaHorizontal);
+                        item.Width -= deltaHorizontal;
+                        break;
+                    case HorizontalAlignment.Right:
+                        deltaHorizontal = Math.Min(-e.HorizontalChange,
+                            item.ActualWidth - item.MinWidth);
+                        item.Width -= deltaHorizontal;
+                        break;
+                }
+
+            }
+            e.Handled = true;
+        }
+    }
+}
